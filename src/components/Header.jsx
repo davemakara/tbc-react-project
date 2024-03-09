5f487a6c585883a012251c27b75bb2ce31c7959f
davidmakarovv <davidmakarr@gmail.com>
2024-03-09 21:15:47 +0400

    burger menu added, header styled and done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import Navigation from "./Navigation";
 
 import outputLogo from "../assets/image/output-onlinepngtools.png";
+import menuIcon from "../assets/image/burger-bar.png";
 
 const Header = () => {
   return (
     <header className="header">
       <img src={outputLogo} alt="logo" className="logo" />
       <Navigation />
+      <img src={menuIcon} alt="menu icon" className="menu-icon" />
     </header>
   );
 };
