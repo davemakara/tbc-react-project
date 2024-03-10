62d1b669bd4ea207f146dd1cc209923454d7db4b
davidmakarovv <davidmakarr@gmail.com>
2024-03-10 11:47:01 +0400

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Fragment>
       <Header />
       <Content />
+      <Footer />
     </Fragment>
   );
 }
