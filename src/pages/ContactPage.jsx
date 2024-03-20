bffc5234ff5f604c94c480e1b03567648a4bad4f
davidmakarovv <davidmakarr@gmail.com>
2024-03-20 23:29:46 +0400

    contact page styled, homepage styles adjusted

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,7 @@
 const ContactPage = () => {
   return (
     <section className="contactPage-wrapper">
-      <div>
+      <div className="contact-info-box">
         <h1>CONTACT US</h1>
         <ul>
           <li>Email: companyinfo@gmail.com</li>
@@ -9,6 +9,51 @@ const ContactPage = () => {
           <li>Phone: +995 555123123</li>
         </ul>
       </div>
+      <form className="contact-form">
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          placeholder="First Name"
+          className="form-input"
+        />
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          placeholder="Last Name"
+          className="form-input"
+        />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email Address"
+          className="form-input"
+        />
+        <input
+          type="number"
+          id="number"
+          name="number"
+          placeholder="Phone Number"
+          className="form-input"
+        />
+        <div className="checkbox-wrapper">
+          <input
+            type="checkbox"
+            id="agree-checkbox"
+            name="agree-checkbox"
+            className="check"
+          />
+          <label htmlFor="agree-checkbox">
+            By clicking the button, I agree with the collection and processing
+            of my personal data as described in the Privacy policy. *
+          </label>
+        </div>
+        <div>
+          <button className="form-submit-btn">SUBMIT</button>
+        </div>
+      </form>
     </section>
   );
 };
