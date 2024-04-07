cf3b07ba2c43ee193f43eb2bdb7ed94bc02aa8ee
davidmakarovv <davidmakarr@gmail.com>
2024-04-07 16:09:13 +0400

    added favicon, changed configuration to build app

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,9 +2,14 @@ import Layout from "../UI/Layout";
 import "../styles/index.css";
 import "../styles/app.css";
 
+import FavIcon from "../public/favicon.png";
+
 export const metadata = {
   title: "TBC App",
   description: "Your first NextJS app!",
+  icons: {
+    icon: "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_favicon_icon_130608.png",
+  },
 };
 
 export default function RootLayout({ children }) {
