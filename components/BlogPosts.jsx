ee6dff53510e86b6c41de54a2a075d01fc671748
davidmakarovv <davidmakarr@gmail.com>
2024-04-11 15:11:34 +0400

    blogposts added

--- components/BlogPosts.jsx
+++ components/BlogPosts.jsx
@@ -4,8 +4,6 @@ import { useState, useEffect } from "react";
 
 import Image from "next/image";
 
-import { blogPosts } from "./store";
-
 const BlogPosts = () => {
   const [blogsData, setBlogsData] = useState([]);
 
