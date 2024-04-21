0a6bc3385a26e1c8d510255b6c5a25380f4a8e55
davidmakarovv <davidmakarr@gmail.com>
2024-04-22 03:37:09 +0400

    testing host issue

--- scripts/login.js
+++ scripts/login.js
@@ -1,12 +1,15 @@
 import { getBaseUrl } from "../utils/getBaseUrl";
 
 export const handleLogin = async (username, password) => {
-  const response = await fetch(`${getBaseUrl()}api/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      username,
-      password,
-    }),
-  });
+  const response = await fetch(
+    `https://tbc-acceleration-project.vercel.app/api/login`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    }
+  );
 };
--- scripts/logout.js
+++ scripts/logout.js
@@ -1,7 +1,10 @@
 import { getBaseUrl } from "../utils/getBaseUrl";
 
 export const handleLogout = async () => {
-  const response = await fetch(`${getBaseUrl()}api/logout`, {
-    method: "POST",
-  });
+  const response = await fetch(
+    `https://tbc-acceleration-project.vercel.app/api/logout`,
+    {
+      method: "POST",
+    }
+  );
 };
