8411f3bb9d99846a48a533cb4eebe2004a639b3f
davidmakarovv <davidmakarr@gmail.com>
2024-05-15 14:34:15 +0400

    moved routes into users folder

--- app/api.ts
+++ app/api.ts
@@ -1,21 +1,21 @@
 import { BASE_URL } from "../constants";
 
 export async function getUsers() {
-  const response = await fetch(BASE_URL + "/api/get-users");
+  const response = await fetch(BASE_URL + "/api/users/get-users");
   const { users } = await response.json();
 
   return users.rows;
 }
 
 export async function createUser(name: string, email: string, age: string) {
-  return await fetch(BASE_URL + "/api/create-user", {
+  return await fetch(BASE_URL + "/api/users/create-user", {
     method: "POST",
     body: JSON.stringify({ name, email, age }),
   });
 }
 
 export async function deleteUser(id: number) {
-  await fetch(`${BASE_URL}/api/delete-user/${id}`, {
+  await fetch(`${BASE_URL}/api/users/delete-user/${id}`, {
     method: "DELETE",
   });
 }
@@ -26,7 +26,7 @@ export async function updateUser(
   email: string,
   age: string
 ) {
-  return await fetch(BASE_URL + "/api/edit-user", {
+  return await fetch(BASE_URL + "/api/users/edit-user", {
     method: "PUT",
     body: JSON.stringify({ id, name, email, age }),
   });
