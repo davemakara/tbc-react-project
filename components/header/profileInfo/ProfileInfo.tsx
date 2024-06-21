77be5ae26279ff9a48d7fb4287ea698c940b8e74
davidmakarovv <davidmakarr@gmail.com>
2024-06-21 19:55:25 +0400

    login button styled

--- components/header/profileInfo/ProfileInfo.tsx
+++ components/header/profileInfo/ProfileInfo.tsx
@@ -6,7 +6,20 @@ import ProfileBtn from "../profile-btn/ProfileBtn";
 const ProfileInfo = () => {
   const { user } = useUser();
 
-  return <>{user ? <ProfileBtn /> : <a href="/api/auth/login">Login</a>}</>;
+  return (
+    <>
+      {user ? (
+        <ProfileBtn />
+      ) : (
+        <a
+          href="/api/auth/login"
+          className="py-1 px-4 bg-red rounded-lg text-md"
+        >
+          Login
+        </a>
+      )}
+    </>
+  );
 };
 
 export default ProfileInfo;
