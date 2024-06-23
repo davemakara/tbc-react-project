2359e9cfdb3c55f28e2f26e4385a973b70170bf2
davidmakarovv <davidmakarr@gmail.com>
2024-06-24 02:24:51 +0400

    stripe added

---
+++ app/api/checkout/route.ts
@@ -0,0 +1,75 @@
+import { NextResponse } from "next/server";
+
+import { BASE_URL } from "../../../constants";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+const getActiveProducts = async () => {
+  const checkProducts = await stripe.products.list();
+  const availableProducts = checkProducts.data.filter(
+    (product: any) => product.active === true
+  );
+  return availableProducts;
+};
+export const POST = async (request: any) => {
+  const { products, user } = await request.json();
+
+  const data: any = products;
+
+  let activeProducts = await getActiveProducts();
+  try {
+    for (const product of data) {
+      const stripeProduct = activeProducts.find(
+        (stripeProduct: any) =>
+          stripeProduct?.name?.toLowerCase() == product?.title?.toLowerCase()
+      );
+      if (stripeProduct == undefined) {
+        await stripe.products.create({
+          name: product.title,
+          default_price_data: {
+            unit_amount: Number(product.price) * 100,
+            currency: "usd",
+          },
+        });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  activeProducts = await getActiveProducts();
+  let stripeItems: any = [];
+
+  for (const product of data) {
+    const stripeProduct = activeProducts?.find(
+      (prod: any) => prod?.name?.toLowerCase() == product?.title?.toLowerCase()
+    );
+
+    if (stripeProduct) {
+      stripeItems.push({
+        price: stripeProduct?.default_price,
+        quantity: product?.quantity,
+      });
+    }
+  }
+  if (stripeItems.length === 0) {
+    throw new Error("No valid items to purchase");
+  }
+  const session = await stripe.checkout.sessions.create({
+    line_items: stripeItems,
+    mode: "payment",
+    customer_email: user.email,
+    payment_intent_data: {
+      metadata: {
+        id: user.sub,
+        // phone: formData.phone_number || userForm?.phone_number,
+        // address: formData.address || userForm?.address,
+      },
+    },
+    success_url: `${BASE_URL}/`,
+    cancel_url: `${BASE_URL}/cart`,
+  });
+
+  console.log("Stripe Session:", session);
+
+  return NextResponse.json({ url: session.url });
+};
